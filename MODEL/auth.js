23c27d108a96964d09e819d85fff080d907168d5
Noor Fatima <noor2239064@gmail.com>
2026-07-24 21:01:19 +0500

    added auth schema

---
+++ MODEL/auth.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const authSchema = new Schema ({
+    username:{
+        type:String,
+        required:true,
+        maxlength:50,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true, 
+        minlenght:8,
+        maxlength:20
+    }
+});
+
+const Auth = mongoose.model('Auth', authSchema);
+module.exports = Auth;
